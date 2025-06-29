4e5d84916291703d6f86801c7c39fbeee8b2bb28
Lorenzo Taglio <l.taglio@studenti.uniba.it>
2025-06-29 15:58:26 +0200

    Change Socket conn

--- client/client.js
+++ client/client.js
@@ -1,7 +1,7 @@
 import { io } from "https://cdn.socket.io/4.7.2/socket.io.esm.min.js";
 
 
-const socket = io("timespot-clone.railway.internal", {
+const socket = io("timespot-clone-production.up.railway.app", {
   transports: ["websocket"],
 });
 
